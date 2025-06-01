863650588a019463d159d7578de64293d604250a
njange <jamesnjange80@gmail.com>
2025-06-01 13:14:41 +0300

    Implement user data streaming from MySQL database

---
+++ python-generators-0x00/0-stream_users.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+def stream_users():
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="your_mysql_username",
+        password="your_mysql_password",
+        database="ALX_prodev"
+    )
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM user_data")
+    
+    for row in cursor:
+        yield row
+
+    cursor.close()
+    connection.close()
